65bb2deefb6f115edb1bc484afd9408de28c0196
Andrew Rizk <97995173+AndrewidRizk@users.noreply.github.com>
2024-11-13 10:39:22 -0500

    Update db_connection.py

--- Database/db_connection.py
+++ Database/db_connection.py
@@ -1,6 +1,6 @@
 import psycopg2
 from . import config
-
+import time
 
 def connect_postgresql():
     return psycopg2.connect(**config.POSTGRESQL_CONFIG)
@@ -9,9 +9,10 @@ def connect_postgresql():
 
 def test_connection():
     try:
-        for i in range(5):
+        for i in range(10):
             if connect_postgresql():
                 print("postgre connection success")
+                time.sleep(0.5)
                 break
     except Exception as e:
         print(f"postgre connection FAILED with an error: {e}")
