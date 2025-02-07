838765e8cc172ff6e5f4ade742fa6d858f016607
Andrewid <Androwmaged47@gmail.com>
2025-02-07 15:20:03 -0500

    fixing connect to database issue

--- Database/db_connection.py
+++ Database/db_connection.py
@@ -3,6 +3,14 @@ from . import config
 import time
 
 def connect_postgresql():
+    for i in range(10):
+        try:
+            conn = psycopg2.connect(**config.POSTGRESQL_CONFIG)
+            if conn:
+                return conn
+        except Exception as e:
+            print(f"postgre connection FAILED with an error: {e}")
+
     return psycopg2.connect(**config.POSTGRESQL_CONFIG)
 
 
